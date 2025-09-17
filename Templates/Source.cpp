f040411e732d946a2db31372c082e379309359d0
Snowy-Cuber <tfischerrenee1@gmail.com>
2025-09-17 13:42:03 -0400

    Completed half templete and int function. Drivers to test the int half function.

--- Templates/Source.cpp
+++ Templates/Source.cpp
@@ -3,16 +3,46 @@
 // 9/17/2025
 
 #include<iostream>
-
+#include<cmath>
 using namespace std;
 
+template<typename T>
+T half(T n) {
+	return n / 2.0;
+}
+
+int half(int);
 
 int main() {
 
+
+	int a = 5;
+	int b = 10;
+	int c = 23;
+	int d = 44;
+
+	cout << half(a) << endl << endl;
+	cout << half(b) << endl << endl;
+	cout << half(c) << endl << endl;
+	cout << half(d) << endl << endl;
 
 
 
 	cout << endl << endl;
 	system("pause");
 	return 0;
+}
+
+int half(int n) {
+	
+	double temp = n / 2.0;
+	int tempInt = n / 2;
+	cout << temp << endl << tempInt << endl;
+	double decimal = temp - tempInt;
+	if (decimal < 0.5) {
+		return tempInt;
+	}
+	else {
+		return tempInt + 1;
+	}
 }
